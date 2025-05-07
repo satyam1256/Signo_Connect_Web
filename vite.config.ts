77cb3f9dc97c3b79d1415acbdb73233a598db52c
Satyam . <satyam.student.min21@itbhu.ac.in>
2025-05-07 20:10:22 +0530

    added vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base:'/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
@@ -33,9 +34,10 @@ export default defineConfig({
   // IMPORTANT: Vite's root is set to the 'client' directory
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
+
   // The problematic 'define' section has been removed.
   // Vite will now automatically load VITE_* variables from the .env file
   // located in the `root` directory (`client` in this case).
